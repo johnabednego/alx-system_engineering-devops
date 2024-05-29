f26dccd7f3a286105da49d53b3b6e390476012c7
johnabednego <jilimajohnabednego@gmail.com>
2024-05-29 01:14:01 +0000

    hmmm

--- 0x14-mysql/replica_user.sql
+++
@@ -1,3 +0,0 @@
-CREATE USER IF NOT EXISTS 'replica_user'@'%' IDENTIFIED BY 'password';
-GRANT REPLICATION SLAVE, REPLICATION CLIENT ON *.* TO 'replica_user'@'%';
-GRANT SELECT ON mysql.`user` TO 'holberton_user'@'localhost';
