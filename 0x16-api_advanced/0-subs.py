19d69fa7570bdcef301a53c7bdcfeb74345aea22
johnabednego <jilimajohnabednego@gmail.com>
2024-06-19 02:12:24 +0000

    final

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python3
-"""
-Script that queries subscribers on a given Reddit subreddit.
-"""
-
+"""Script that returns no. of subscribers of a subreddit"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
+    """Function that returns no. of subs of a subreddit"""
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+
+    headers = {'User-Agent': 'selBot/1.0'}
+    URL = f'https://www.reddit.com/r/{subreddit}/about.json'
+
+    try:
+        response = requests.get(URL, headers=headers, allow_redirects=False)
+        response.raise_for_status()
         data = response.json()
         subscribers = data['data']['subscribers']
         return subscribers
-    else:
+
+    except requests.exceptions.RequestException:
+        return 0
+
+    except (KeyError, ValueError):
         return 0
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,24 +1,33 @@
 #!/usr/bin/python3
-'''
-    this module contains the function top_ten
-'''
+"""Script that returns top 10 hot posts of a subreddit"""
 import requests
-from sys import argv
 
 
 def top_ten(subreddit):
-    '''
-        returns the top ten posts for a given subreddit
-    '''
-    user = {'User-Agent': 'Lizzie'}
-    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
-                       .format(subreddit), headers=user).json()
+    """Function that prints top 10 posts of a subreddit"""
+    if subreddit is None or not isinstance(subreddit, str):
+        print(None)
+        return
+
+    headers = {'User-Agent': 'selBot/2.0'}
+    URL = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
+
     try:
-        for post in url.get('data').get('children'):
-            print(post.get('data').get('title'))
-    except Exception:
+        response = requests.get(URL, headers=headers, allow_redirects=False)
+        response.raise_for_status()
+        data = response.json()
+        posts = data['data']['children']
+
+        if not posts:
+            print("None")
+
+        else:
+            for post in posts:
+                title = post['data']['title']
+                print(title)
+
+    except requests.exceptions.RequestException:
         print(None)
 
-
-if __name__ == "__main__":
-    top_ten(argv[1])
+    except (KeyError, ValueError):
+        print(None)
